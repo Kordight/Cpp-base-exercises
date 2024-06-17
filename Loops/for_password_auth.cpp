8238335f2e36dcd69833b5fd60204a012b580252
seba0456 <46867564+seba0456@users.noreply.github.com>
2024-06-17 18:47:27 +0200

    Update for_password_auth.cpp

--- Loops/for_password_auth.cpp
+++ Loops/for_password_auth.cpp
@@ -1,26 +1,35 @@
-//Task:  Write a password verification program that gives the user only a few chances to enter the correct password, making it difficult to use a password cracker.
+// Task:  Write a password verification program that gives the user only a few chances to enter the correct password, making it difficult to use a password cracker.
 #include <iostream>
 #include <string>
 using namespace std;
 
-int main(){
-    //Config these
+int main()
+{
+    // Config these
     int tries = 5;
-    string password ="qwerty";
+    string password = "qwerty";
+    cout << "Enter your password:";
     for (int i = 0; i <= tries; i++)
     {
-        
+
         string userInput;
         cin >> userInput;
-        if (password == userInput){
+        if (password == userInput)
+        {
             cout << "Password is valid.\n";
             return 0;
         }
         else
         {
-            cout << "Invalid password!" << tries - i << " tries remain!";
+            if ((tries - i) > 0)
+            {
+                cout << "Invalid password!" << tries - i << " tries remain!";
+            }
+            else
+            {
+                cout << "All tries has been used!\n";
+            }
         }
     }
-    cout << "All tries has been used!\n";
     return 0;
 }
