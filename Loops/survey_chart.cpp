b302f4d00f2da1579e916c34b6f9c96a0cc9ec83
seba0456 <46867564+seba0456@users.noreply.github.com>
2024-06-17 19:00:55 +0200

    Create survey_chart.cpp

---
+++ Loops/survey_chart.cpp
@@ -0,0 +1,13 @@
+/*
+Task: Write a program that provides an option to sum the results of a survey in which there can be three different values.
+    The input entered into the program is a survey question and three possible answers.
+    The first answer is assigned a value of 1,
+    the second is assigned a value of 2,
+    and the third is assigned a value of 3.
+    The answers are summed until the user enters then the program should show the results of the survey.
+    Try to generate a bar chart, showing the results scaled to fit on the screen regardless of the number of answers given.
+
+Possible solution: i < ( ( var * scale ) / max )
+*/
+
+//TO DO Later
