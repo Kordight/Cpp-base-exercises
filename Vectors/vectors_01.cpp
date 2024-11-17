52a453654ddf62480616685ffccc4da88c7bc1d2
Kordight <164764949+Kordight@users.noreply.github.com>
2024-11-17 14:53:18 +0100

    Create vectors_01.cpp

---
+++ Vectors/vectors_01.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+void print_vector(const vector<double> &numbers);
+void print_text_in_frame(const string &text_to_display, const int &frame_width)
+{
+    int message_length = text_to_display.size() + 2 * frame_width + 4;
+    string line(message_length, '*');
+    string border_width(frame_width, '*');
+    string message = line + "\n";
+    message += border_width + "  " + text_to_display + "  " + border_width + "\n";
+    message += line + "\n";
+    cout << message;
+}
+int main()
+{
+    vector<double> numbers{3.14, 25, 35, 37.21, 23.05};
+    cout << "Enter number to add" << endl;
+    double number_to_add = 0;
+    cin >> number_to_add;
+    numbers.push_back(number_to_add);
+    // Reading
+    print_vector(numbers);
+    // Modify
+    size_t vector_index = 0;
+    for (auto &num : numbers)
+    {
+        if (vector_index % 2 == 0)
+        {
+            num *= 1.2;
+            cout << "Modified index: " << vector_index << endl;
+        }
+        vector_index++;
+    }
+    print_text_in_frame("Modifying vector", 3);
+    print_vector(numbers);
+    return 0;
+}
+void print_vector(const vector<double> &numbers)
+{
+    for (auto num : numbers)
+    {
+        cout << "Number: " << num << ".\n";
+    }
+}
